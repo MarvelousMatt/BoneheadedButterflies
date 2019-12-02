292e6290b7750b8ef81188a3587d75f45b6faa11
Matt J <MJ209069@falmouth.ac.uk>
2019-12-02 17:55:42 +0000

    Began implementation on vision sphere for butterflies

--- Assets/Scripts/VisionSphere.cs
+++ Assets/Scripts/VisionSphere.cs
@@ -6,14 +6,14 @@ public class VisionSphere : MonoBehaviour
 {
     //Adds all seen objects to a list, prioritises these objects and sends a response to the parent butterfly
 
-    /*
+  
     //List of all gameobjects in the trigger
     List<GameObject> inTriggerList = new List<GameObject>();
 
     private void OnTriggerEnter(Collider other)
     {
         //Is it food, breedable butterflies and is it not right next to us
-        if (other.CompareTag("Flower") || other.CompareTag("Breedable") && Vector3.Distance(transform.position, other.transform.position) > 2)
+        if (other.CompareTag("Flower") || other.CompareTag("Breedable"))
             inTriggerList.Add(other.gameObject);
     }
 
@@ -22,39 +22,21 @@ public class VisionSphere : MonoBehaviour
 
         for (int i = 0; i < inTriggerList.Count; i++)
         {
-            if (other.gameObject == player)
+            if (inTriggerList.Contains(other.gameObject))
             {
                 inTriggerList.Remove(other.gameObject);
-                isSeeingPlayer = false;
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject RequestTarget()
     {
-        //Debug.Log(inTriggerList.Count.ToString());
-
-        if (isSensing == true)
-        {
-            for (int i = 0; i < inTriggerList.Count; i++)
-            {
-                //Debug.Log(isSeeingPlayer + " " + inTriggerList[i].name);
-
-                if (player == inTriggerList[i])
-                {
-                    isSeeingPlayer = true;
-                    break;
-                }
-                else
-                {
-                    isSeeingPlayer = false;
-                }
-            }
-        }
 
 
+
+        return null;
     }
+    
 
-    */
+    
 }
